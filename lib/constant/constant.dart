819a23a3d60f45699dbc32de049bd34a24bbd72f
Deyaanoor <127193661+Deyaanoor@users.noreply.github.com>
2026-02-15 23:08:45 +0200

    fix: Update server URL in AppConstants for correct API endpoint

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,7 +3,8 @@
 class AppConstants {
   static const developerName = 'م. ضياء بني جابر';
   static const developerEmail = 'deyaanoor9@gmail.com';
-  static const developerPhone = '970595352667+';
-  static const serverUrl = 'https://serverpos.onrender.com/activate';
+  static const developerPhone = '+970595352667';
+  static const serverUrl =
+      'https://server-al-motamayez.onrender.com/api/activate';
   static const secretKey = 'MOTAMAYEZ_PRIVATE_KEY_2026';
 }
